57c5a18c41377d120dfcdafd057c137c25921944
rc0034163 <rc0034163@CRO-24-09>
2019-02-25 14:55:48 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double d1 = 5e6;
+            richTextBox2.AppendText(d1.ToString() + "\n");
+            richTextBox2.AppendText(d1.ToString("N") + "\n");
+            richTextBox2.AppendText(d1.ToString("n3") + "\n");
+            richTextBox2.AppendText(d1.ToString("n0") + "\n");
+            richTextBox2.AppendText(d1.ToString("0") + "\n");
+            richTextBox2.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox2.AppendText(d1.ToString("0.E0") + "\n");
+            richTextBox2.AppendText(d1.ToString("0.00E00") + "\n");
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
